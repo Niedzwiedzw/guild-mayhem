0bd05886bc633a19982ab529506f7eba284a6fd4
niedzwiedzw <niedzwiedzwo@gmail.com>
2021-10-28 22:58:21 +0200

    physics dont work

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use bevy::prelude::*;
 pub mod player;
 
+use bevy::prelude::*;
+use heron::prelude::*;
+
 fn main() {
-    App::new()
+    App::build()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(player::PlayerPlugin)
-        .add_startup_system(setup)
+        .add_plugin(PhysicsPlugin::default())
+        .insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0)))
+        .add_startup_system(setup.system())
         .run();
 }
 
@@ -21,16 +25,30 @@ fn setup(
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..Default::default()
-    });
+    })
+        .insert(
+            RigidBody::Static
+        )
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(5.0, 1.0, 5.0) / 2.0,
+            border_radius: None,
+        });
     // cube
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..Default::default()
-    });
+    })
+        .insert(
+            RigidBody::Dynamic
+        )
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(1.0, 1.0, 1.0) / 2.0,
+            border_radius: None,
+        });
     // light
-    commands.spawn_bundle(PointLightBundle {
+    commands.spawn_bundle(LightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..Default::default()
     });
--- src/player.rs
+++ src/player.rs
@@ -1,25 +1,32 @@
+use bevy::{
+    app::{Events, ManualEventReader},
+    input::{keyboard, mouse::MouseMotion},
+    prelude::*,
+    render::camera::Camera,
+};
+use heron::prelude::*;
 use std::ops::Add;
-
-use bevy::{app::{Events, ManualEventReader}, input::{keyboard, mouse::MouseMotion}, prelude::*, render::camera::Camera};
 
 pub struct PlayerPlugin;
 
-#[derive(Component)]
 pub struct Player;
 
-#[derive(Component)]
-pub struct Position(Vec3);
-
-#[derive(Component)]
 pub struct Orientation(Quat);
 
 pub fn spawn_player(mut commands: Commands) {
     let position = Vec3::new(-2.0, 2.5, 5.0);
     let orientation = Quat::IDENTITY;
-    commands.spawn()
+    commands
+        .spawn()
         .insert(Player)
-        .insert(Position(position))
-        .insert(Orientation(orientation));
+        .insert(Transform::from_translation(position))
+        .insert(Acceleration::from_linear(Vec3::ZERO))
+        .insert(Orientation(orientation))
+        .insert(RigidBody::Dynamic)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(1.0, 1.0, 1.0) / 2.0,
+            border_radius: None,
+        });
 
     // camera
     commands.spawn_bundle(PerspectiveCameraBundle {
@@ -29,34 +36,38 @@ pub fn spawn_player(mut commands: Commands) {
 }
 
 pub fn camera_follow_system(
-    player: Query<(&Player, &Position, &Orientation)>,
-    mut camera: Query<(&Camera, &mut Transform)>
+    player: Query<(&Player, &Transform, &Orientation), Without<Camera>>,
+    mut camera: Query<(&Camera, &mut Transform), Without<Player>>,
 ) {
-    let (_, player_position, player_orientation) = player.single();
-    let (_, mut camera_position) = camera.single_mut();
-    *camera_position = Transform::from_translation(player_position.0);
-    camera_position.rotation = player_orientation.0
+    if let Ok((_, player_position, player_orientation)) = player.single() {
+        if let Ok((_, mut camera_position)) = camera.single_mut() {
+            *camera_position = *player_position;
+            camera_position.rotation = player_orientation.0
+        }
+    }
 }
 
 pub fn player_controls_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Position, &Orientation)>
+    mut query: Query<(&Player, &mut Acceleration, &Orientation)>,
 ) {
-    let (_, mut position, orientation) = query.single_mut();
-    let mut velocity = Vec3::ZERO;
-    if keyboard_input.pressed(KeyCode::W) {
-        velocity.z -= 0.4;
+    if let Ok((_, mut acceleration, orientation)) = query.single_mut() {
+        let mut acc = Vec3::ZERO;
+        if keyboard_input.pressed(KeyCode::W) {
+            acc.z -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            acc.z += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            acc.x += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            acc.x -= 1.0;
+        }
+
+        *acceleration = Acceleration::from_linear(orientation.0.mul_vec3(acc) + acceleration.linear);
     }
-    if keyboard_input.pressed(KeyCode::S) {
-        velocity.z += 0.4;
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        velocity.x += 0.4;
-    }
-    if keyboard_input.pressed(KeyCode::A) {
-        velocity.x -= 0.4;
-    }
-    position.0 += orientation.0.mul_vec3(velocity);
 }
 
 #[derive(Default)]
@@ -111,9 +122,8 @@ fn player_look_system(
 }
 
 impl Plugin for PlayerPlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .init_resource::<InputState>()
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<InputState>()
             .add_startup_system(spawn_player.system())
             .add_startup_system(initial_grab_cursor.system())
             .add_system(player_controls_system.system())
